3547ba16c785464fd1651f8bbf744a5963f83f7f
shiggsehhh <21am137@stjosephs.ac.in>
2025-06-16 23:29:42 +0530

    Add files via upload

---
+++ customer.sql
@@ -0,0 +1,24 @@
+use banking_system1;
+show tables;
+describe users;
+INSERT INTO accounts (
+    user_id,
+    account_number,
+    account_type,
+    balance,
+    status,
+    created_at,
+    updated_at
+) VALUES (
+    1,
+    'ACC1234567890',
+    'Savings',
+    10000.00,
+    'Active',
+    '2025-06-13 08:50:00',
+    '2025-06-13 08:50:00'
+);
+
+
+
+
